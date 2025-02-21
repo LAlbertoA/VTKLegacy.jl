938017d93255a6c51d4364ca9ea29f2329f3f956
LAlbertoA <luis.alberto@ciencias.unam.mx>
2025-02-21 03:18:55 -0600

    Correct test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ using Test
     m = LoadVTK("../VTK_examples/StructuredPointsExample.vtk");
     um = LoadVTK("../VTK_examples/UnstructuredGridExample.vtk");
     @test typeof(m) == StructuredPoints
-    @test m.data.size == (length(m.dataNames), m.nx, m.ny, m.nz)
+    @test size(m.data) == (length(m.dataNames), m.nx, m.ny, m.nz)
     @test length(unique(m.dataAttributes)) == 2
     @test count(x->x=="VECTORS",m.dataAttributes)*3+count(x->x=="SCALARS",m.dataAttributes) == length(m.dataNames)
 
     @test typeof(um) == UnstructuredGrid
-    @test um.cellData.size == (length(um.cellDataNames), um.ncells)
+    @test size(um.cellData) == (length(um.cellDataNames), um.ncells)
     @test count(x->x=="VECTORS",um.cellDataAttributes)*3+count(x->x=="SCALARS",um.cellDataAttributes) == length(um.cellDataNames)
 end
