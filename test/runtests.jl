f9ed62a0d483a657f37c4ef299c99f3253988fd7
LAlbertoA <luis.alberto@ciencias.unam.mx>
2025-02-25 00:43:16 -0600

    Correct tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,8 @@ using Test
     m = LoadVTK("../VTK_examples/StructuredPointsExample.vtk");
     um = LoadVTK("../VTK_examples/UnstructuredGridExample.vtk");
     @test typeof(m) == StructuredPoints
-    @test size(m.data) == (length(m.dataNames), m.nx, m.ny, m.nz)
+    @test size(m.data) == (count(x->x=="VECTORS",m.dataAttributes)*3+count(x->x=="SCALARS",m.dataAttributes), m.nx, m.ny, m.nz)
     @test length(unique(m.dataAttributes)) == 2
-    @test count(x->x=="VECTORS",m.dataAttributes)*3+count(x->x=="SCALARS",m.dataAttributes) == length(m.dataNames)
 
     @test typeof(um) == UnstructuredGrid
     @test size(um.cellData) == (length(um.cellDataNames), um.ncells)
